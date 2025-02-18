a585b7947e663bfa969464d2843dfd17b4df870b
sambit826059 <sambitmohanty19472gmail.com>
2025-02-18 15:24:12 +0530

    added the GET gadgets route

--- src/routes/gadgets.ts
+++ src/routes/gadgets.ts
@@ -1,12 +1,44 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { randomNameGenerator } from "../utils/randomNameGenerator";
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
-  res.send("Gadget server healthy");
+interface GadgetName {
+  name: string;
+}
+
+interface ResponseData {
+  namesWithChances: string[];
+}
+
+router.get("/", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const gadgetNames = await prisma.gadgets.findMany({
+      select: {
+        name: true,
+      },
+    });
+
+    const namesWithChances: string[] = gadgetNames.map(({ name }) => {
+      const capitalizedName = name[0].toUpperCase() + name.slice(1);
+      const percentage = Math.floor(Math.random() * 100);
+
+      return (
+        "The " + capitalizedName + " - " + percentage + "% success probability"
+      );
+    });
+
+    return res.status(200).json({
+      namesWithChances,
+    });
+  } catch {
+    console.error("Error getting the gadget names");
+    res
+      .status(500)
+      .json({ error: "Failed to get the gadget names from the databse" });
+  }
 });
 
 router.post("/", async (req, res): Promise<any> => {
