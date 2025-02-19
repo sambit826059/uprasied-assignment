5a7a788bfcee13e8ae0e4407a448d20e4da8ed96
sambit826059 <sambitmohanty19472gmail.com>
2025-02-19 17:22:32 +0530

    remove api call and added manual name generation

--- src/utils/randomNameGenerator.ts
+++ src/utils/randomNameGenerator.ts
@@ -1,15 +1,57 @@
-export const randomNameGenerator = async () => {
-  try {
-    const response = await fetch("https://random-word-api.herokuapp.com/word");
-    if (!response.ok) {
-      throw new Error(
-        `HTTP error creating random name! status: ${response.status}`,
-      );
-    }
-    const data = await response.json();
-    const randomNum = Math.floor(1000 + Math.random() * 9999);
-    return data[0] + randomNum;
-  } catch (error) {
-    console.error("Error generating random name: ", error);
-  }
+export const randomNameGenerator = (): string => {
+  const adjectives: string[] = [
+    "swift",
+    "silent",
+    "brave",
+    "mystic",
+    "lucky",
+    "fierce",
+    "bold",
+    "rapid",
+    "graceful",
+    "nimble",
+    "elegant",
+    "daring",
+    "vibrant",
+    "resolute",
+    "majestic",
+    "sly",
+    "proud",
+    "radiant",
+    "vigilant",
+    "audacious",
+  ];
+
+  const nouns: string[] = [
+    "panther",
+    "tiger",
+    "falcon",
+    "dragon",
+    "whale",
+    "phoenix",
+    "lion",
+    "eagle",
+    "shark",
+    "wolf",
+    "bear",
+    "cobra",
+    "leopard",
+    "griffin",
+    "hyena",
+    "viper",
+    "ox",
+    "cheetah",
+    "stallion",
+    "ram",
+  ];
+
+  // Genetating a new name from above arrays
+  const randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+  // Random number generation
+  const randomNum = Math.floor(1000 + Math.random() * 9000);
+
+  return `${randomAdjective}-${randomNoun}-${randomNum}`;
 };
